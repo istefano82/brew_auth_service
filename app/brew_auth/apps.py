1fd2e33867b5b1641dcf7f30a63bc8297707d713
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2020-04-07 02:23:12 +0300

    inital authetication app skeleton

---
+++ app/brew_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BrewAuthConfig(AppConfig):
+    name = 'brew_auth'
---
+++ app/brew_auth/tests/test_views.py
@@ -0,0 +1,6 @@
+# from django.test import TestCase
+# from django.test import client
+
+# class TestAuthApiView(TestCase):
+
+#     pass
