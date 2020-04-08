8fc98c059d67d43d42ac2006f445e43bbce6ba67
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2020-04-08 21:13:10 +0300

    Register brew_auth app, swagger docs and jwt tokens

--- app/backend_settings/urls.py
+++ app/backend_settings/urls.py
@@ -13,14 +13,44 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 from django.conf.urls import url
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from rest_framework_jwt.views import (obtain_jwt_token, refresh_jwt_token,
+                                      verify_jwt_token)
+
+...
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v0',
+        description="Brew Authentication API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+drf_yasg_urls = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger',
+                                           cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc',
+                                         cache_timeout=0), name='schema-redoc'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api-token-auth/', obtain_jwt_token),
-    url(r'^api-token-refresh/', refresh_jwt_token),
-    url(r'^api-token-verify/', refresh_jwt_token),
+    url(r'^api-token-refresh/', refresh_jwt_token, name='refresh_jwt_token'),
+    url(r'^api-token-verify/', verify_jwt_token, name='verify_jwt_token'),
+    path('api/v0/brew_auth/', include('brew_auth.urls'), name='brew_auth'),
 ]
+
+urlpatterns += drf_yasg_urls
